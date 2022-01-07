9cc6ab00259c2ed513954fd39cd8a1cefed5bb8c
Souliya PPS <souliyappsdev@gmail.com>
2022-01-07 10:30:14 +0700

    buddha

--- config/index.js
+++ config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXTJS_PUBLIC_URL || 'http://localhost:3000' || 'https://nextjs-api.vercel.app';
+export const API_URL = process.env.NEXTJS_PUBLIC_URL || 'https://nextjs-api.vercel.app';
